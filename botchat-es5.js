09d639d52fb63a47dd97e0b4a42bcf8499aae2a4
Simon Loynes <simon@askproducts.co>
2018-07-23 10:23:37 +0100

    build

--- botchat-es5.js
+++ botchat-es5.js
@@ -21704,7 +21704,7 @@ var ShellContainer = (function (_super) {
             React.createElement("button", { className: sendButtonClassName, onClick: function () { return _this.onClickSend(); }, "aria-label": this.props.strings.send, role: "button", onKeyPress: function (evt) { return _this.handleSendButtonKeyPress(evt); }, tabIndex: 0, type: "button", disabled: !this.props.apUi.inputState },
                 React.createElement("svg", null,
                     React.createElement("polygon", { id: "Combined-Shape", points: "5.01141071 6 5 14.1666484 22.1428571 16.5 5 18.8333516 5.01141071 27 29 16.5" }))),
-            React.createElement("button", { className: micButtonClassName, onClick: function () { return _this.onClickMic(); }, "aria-label": this.props.strings.speak, role: "button", tabIndex: 0, type: "button", disabled: !this.props.apUi.inputState },
+            React.createElement("button", { className: micButtonClassName, "aria-label": this.props.strings.speak, role: "button", tabIndex: 0, type: "button", disabled: !this.props.apUi.inputState },
                 React.createElement("svg", { width: "28", height: "22", viewBox: "0 0 58 58" },
                     React.createElement("path", { d: "M 44 28 C 43.448 28 43 28.447 43 29 L 43 35 C 43 42.72 36.72 49 29 49 C 21.28 49 15 42.72 15 35 L 15 29 C 15 28.447 14.552 28 14 28 C 13.448 28 13 28.447 13 29 L 13 35 C 13 43.485 19.644 50.429 28 50.949 L 28 56 L 23 56 C 22.448 56 22 56.447 22 57 C 22 57.553 22.448 58 23 58 L 35 58 C 35.552 58 36 57.553 36 57 C 36 56.447 35.552 56 35 56 L 30 56 L 30 50.949 C 38.356 50.429 45 43.484 45 35 L 45 29 C 45 28.447 44.552 28 44 28 Z" }),
                     React.createElement("path", { id: "micFilling", d: "M 28.97 44.438 L 28.97 44.438 C 23.773 44.438 19.521 40.033 19.521 34.649 L 19.521 11.156 C 19.521 5.772 23.773 1.368 28.97 1.368 L 28.97 1.368 C 34.166 1.368 38.418 5.772 38.418 11.156 L 38.418 34.649 C 38.418 40.033 34.166 44.438 28.97 44.438 Z" }),
@@ -26075,6 +26075,7 @@ var DelaySubscriber = (function (_super) {
             this.schedule(state, delay_1);
         }
         else {
+            this.unsubscribe();
             source.active = false;
         }
     };
