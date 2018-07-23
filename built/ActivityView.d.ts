c447676671dfac8ac53ab5dea1ae407a20768a26
Simon Loynes <simon@askproducts.co>
2018-07-23 14:29:50 +0100

    new build

---
+++ built/ActivityView.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="react" />
+import * as React from 'react';
+import { Activity } from 'botframework-directlinejs';
+import { FormatState, SizeState } from './Store';
+import { IDoCardAction } from './Chat';
+export interface ActivityViewProps {
+    format: FormatState;
+    size: SizeState;
+    activity: Activity;
+    onCardAction: IDoCardAction;
+    onImageLoad: () => void;
+    interactive?: Boolean;
+}
+export declare class ActivityView extends React.Component<ActivityViewProps, {}> {
+    constructor(props: ActivityViewProps);
+    shouldComponentUpdate(nextProps: ActivityViewProps): boolean;
+    render(): JSX.Element;
+}
